7f195e33b875befe4fd0f9149ffb5941e3ef44c2
Nabia Lavala <nabialavala@gmail.com>
2026-04-09 00:13:46 -0400

    Implemented working change password feature

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,6 +5,70 @@ import 'authentication_screen.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+  //change password  logic
+  void _showChangePasswordDialog(BuildContext context, AuthService authService) {
+    final TextEditingController _newPasswordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Change Password"),
+          content: TextField(
+            controller: _newPasswordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: "New Password",
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final newPassword = _newPasswordController.text.trim();
+
+                if(newPassword.length < 6) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Password maust be at least 6 characters."),
+                    ),
+                  );
+                  return;
+                }
+
+                try {
+                  await authService.changePassword(newPassword);
+                  if (!context.mounted) return;
+
+                  Navigator.pop(context);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Password updated."),
+                    ),
+                  );
+                } catch (e) {
+                  if (!context.mounted) return;
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("Error: $e"),
+                    ),
+                  );
+                }
+              },
+              child: const Text("Update"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final authService = AuthService();
@@ -34,8 +98,8 @@ class ProfileScreen extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () async {
-                await authService.changePassword("newPassword123");
+              onPressed: () {
+                _showChangePasswordDialog(context, authService);
               },
               child: const Text("Change Password"),
             ),
